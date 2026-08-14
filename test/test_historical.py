bbb4709be8a09d2e1c55f36949a7b5cc502fac6b
Alex <alexisvascot@hotmail.com>
2026-08-14 16:22:07 -0500

    feat(historical): agrega datasets municipales (movilidad, fauna urbana/ambiente, salud) y tests

--- test/test_historical.py
+++ test/test_historical.py
@@ -4,23 +4,32 @@ from src.data.historical_loader import HistoricalDataLoader
 def test_cargar_dataset_educacion():
     loader = HistoricalDataLoader()
     historico = loader.cargar_csv("educacion_unidades_educativas.csv")
-    
     assert len(historico) == 4
     assert historico[0].anio == 2022
     assert historico[0].valor == 12.0
-    assert historico[0].nombre_fuente == "SERCOP"
 
 
-def test_buscar_contexto_por_categoria():
+def test_buscar_contexto_movilidad():
     loader = HistoricalDataLoader()
-    resultado = loader.buscar_contexto_historico(categoria="educacion", objeto="unidades educativas")
-    
-    assert len(resultado) > 0
+    resultado = loader.buscar_contexto_historico(categoria="movilidad", objeto="paradas y buses")
+    assert len(resultado) == 4
     assert resultado[0].unidad == "unidades"
+
+
+def test_buscar_contexto_ambiente_animal():
+    loader = HistoricalDataLoader()
+    resultado = loader.buscar_contexto_historico(categoria="animal", objeto="esterilizaciones")
+    assert len(resultado) == 4
+    assert resultado[0].valor == 15000.0
+
+
+def test_buscar_contexto_salud():
+    loader = HistoricalDataLoader()
+    resultado = loader.buscar_contexto_historico(categoria="salud", objeto="puntos de atencion medica")
+    assert len(resultado) == 4
 
 
 def test_buscar_contexto_no_existente_retorna_vacio():
     loader = HistoricalDataLoader()
-    resultado = loader.buscar_contexto_historico(categoria="transporte", objeto="tren bala")
-    
+    resultado = loader.buscar_contexto_historico(categoria="espacio", objeto="viajes a marte")
     assert resultado == []
